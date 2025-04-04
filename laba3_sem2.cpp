34a36714cb445527ed00683ccb4ae96bf9480fe6
Polinkachan1 <118909072+Polinkachan1@users.noreply.github.com>
2025-04-05 00:25:51 +0300

    final version of project

--- laba3_sem2.cpp
+++ laba3_sem2.cpp
@@ -15,9 +15,9 @@ void display_menu() {
     cout << "1. Введите выражение." << endl;
     cout << "2. Реализует преобразование введенного выражения в прямую или в обратную польскую нотацию" << endl;
     cout << "3. Реализует проверку на корректность простого выражения и выражения, записанного в прямой и обратной польских нотациях" << endl;
-    cout << "4. Реализует вычисления простого выражения и выражения, записанного в прямой польской нотациях. " << endl;
-    cout << "5. Реализует вычисления простого выражения и выражения, записанного в обратной польских нотациях" << endl;
-    cout << "6. ИДЗ" << endl;
+    cout << "4. Реализует вычисления простого выражения." << endl;
+    cout << "5. Реализует вычисления выражения, записанного в обратной польской нотации" << endl;
+    cout << "6. Реализует вычисления выражения, записанного в прямой польской нотации" << endl;
     cout << "7. Выход!" << endl;
 }
 string change_unknown(string expression,int val, char elem) {
@@ -88,12 +88,12 @@ int priority_of_operations(char elem) {
 char top(Stack* top_elem) {
     return top_elem?top_elem->oper:'\n';
 }
-int for_calc(Stack*& stack,char elem, int right, int left) {
+int for_calc(char elem, int right, int left) {
     int result = 0;
     switch (elem) {
     case '+':
         result = left + right;
-        cout << right << "  +  " << left << " = " << result << endl;
+        cout << left << "  +  " << right << " = " << result << endl;
         break;
     case '-':
         result = left - right;
@@ -200,10 +200,186 @@ string make_direct_polish_notation(string expression) {
     reverse(output.begin(), output.end());
     return output;
 }
-void checking_simple_expression(string expression) {}
-void checking_reverse_polish_notation(string expression) {}
-void checking_direct_polish_notation(string expression) {}
-void calculate_simple_expression(string expression) {}
+bool checking_reverse_polish_notation(string expression) {
+    Stack* stack = nullptr;
+    string value;
+    int oper_count = 0;
+    for (int i = 0; i < expression.length(); i++) {
+        char elem = expression[i];
+        if (elem == ' ') {
+            continue;
+        }
+        if (isdigit(elem)) {
+            string value = "";
+            while (isdigit(expression[i])) {
+                value += expression[i];
+                i++;
+            }
+            int res = stoi(value);
+            push(stack, res);
+            oper_count++;
+            i--;
+        }
+        else if (elem == '+' || elem == '-' || elem == '*' || elem == '/') {
+            if (oper_count < 2) {
+                return false;
+            }
+            int right = pop(stack);
+            int left = pop(stack);
+            int result = for_calc(elem, right, left);
+            push(stack, result);
+            oper_count--;
+        }
+        else {
+            return false;
+        }
+    }
+    return oper_count == 1 && (stack != nullptr);
+}
+bool checking_simple_expression(string expression) {
+    Stack* stack = nullptr;
+    int oper_count = 0;
+    bool is_oper = true;
+    for (int i = 0; i < expression.length(); i++) {
+        char elem = expression[i];
+        if (elem == ' ') {
+            continue;
+        }
+
+        if (isdigit(elem)) {
+            while (isdigit(expression[i])) {
+                i++;
+            }
+            oper_count++;
+            is_oper = false;
+            i--;
+        }
+        else if (elem == '(') {
+            push_op(stack, elem);
+            is_oper = true;
+
+        }
+        else if (elem == ')') {
+            while (stack == nullptr || top(stack) != '(') {
+                return false;
+            }
+            pop_op(stack);
+            is_oper = false;
+        }
+        else if (elem == '+' || elem == '-' || elem == '*' || elem == '/') {
+            if (is_oper) {
+                return false;
+            }
+            if (oper_count < 1) {
+                return false;
+            }
+            oper_count--;
+            is_oper = true;
+        }
+        else {
+            return false;
+        }
+    }
+    return oper_count == 1 && (stack == nullptr) && !is_oper;
+}
+bool checking_direct_polish_notation(string expression) { 
+    Stack* stack = nullptr;
+    string value;
+    int oper_count = 0;
+    for (int i = expression.length()-1; i >=0 ; i--) {
+        char elem = expression[i];
+        if (elem == ' ') {
+            continue;
+        }
+        if (isdigit(elem)) {
+            string value = "";
+            while (isdigit(expression[i])) {
+                value += expression[i];
+                i--;
+            }
+            reverse(value.begin(), value.end());
+            int res = stoi(value);
+            push(stack, res);
+            oper_count++;
+            i++;
+        }
+        else if (elem == '+' || elem == '-' || elem == '*' || elem == '/') {
+            if (oper_count < 2) {
+                return false;
+            }
+            int right = pop(stack);
+            int left = pop(stack);
+            int result = for_calc(elem, left, right);
+            push(stack, result);
+            oper_count--;
+        }
+        else {
+            return false;
+        }
+    }
+    return oper_count == 1 && (stack != nullptr);
+}
+int calculate_simple_expression(string expression) {
+    Stack* stack_for_nums = nullptr;
+    Stack* stack_for_opers = nullptr;
+    string value;
+    for (int i = 0; i < expression.length(); i++) {
+        char elem = expression[i];
+        if (elem == ' ') {
+            continue;
+        }
+        if (isdigit(elem)) {
+            value = "";
+            while (isdigit(expression[i])) {
+                value += expression[i];
+                i++;
+            }
+            int res = stoi(value);
+            push(stack_for_nums, res);
+            i--;
+        }
+        else if (elem == '(') {
+            push_op(stack_for_opers, elem);
+
+        }
+        else if (elem == ')') {
+            while (stack_for_opers != nullptr && stack_for_opers->oper != '(') {
+                char oper = pop_op(stack_for_opers);
+                int right = pop(stack_for_nums);
+                int left = pop(stack_for_nums);
+                int result = for_calc(oper, right, left);
+                push(stack_for_nums, result);
+            }
+            pop_op(stack_for_opers);
+        }
+        else if (elem == '+' || elem == '-' || elem == '*' || elem == '/') {
+            while (stack_for_opers != nullptr && priority_of_operations(stack_for_opers->oper) >= priority_of_operations(elem)) {
+                char oper = pop_op(stack_for_opers);
+                int right = pop(stack_for_nums);
+                int left = pop(stack_for_nums);
+                int result = for_calc(oper, right, left);
+                push(stack_for_nums, result);
+            }
+            push_op(stack_for_opers, elem);
+        }
+    }
+    while (stack_for_opers != nullptr) {
+        char oper = pop_op(stack_for_opers);
+        if (stack_for_nums == nullptr) {
+            cout << "Ошибка: Стек чисел пуст!";
+            break;
+        }
+        int right = pop(stack_for_nums);
+        if (stack_for_nums == nullptr) {
+            cout <<"Ошибка: Стек чисел пуст!";
+            break;
+        }
+        int left = pop(stack_for_nums);
+        int result = for_calc(oper, left, right);
+        push(stack_for_nums, result);
+    }
+    return pop(stack_for_nums);
+}
 int calculate_expression_in_reverse_polish_notation(string expression) {
     Stack* stack = nullptr;
     string value;
@@ -218,6 +394,7 @@ int calculate_expression_in_reverse_polish_notation(string expression) {
                 value += expression[i];
                 i++;
             }
+
             int res = stoi(value);
             push(stack, res);
             i--;
@@ -225,7 +402,7 @@ int calculate_expression_in_reverse_polish_notation(string expression) {
         else if (elem == '+' || elem == '-' || elem == '*' || elem == '/') {
             int right = pop(stack);
             int left = pop(stack);
-            int result = for_calc(stack, elem, right,left);
+            int result = for_calc(elem, right,left);
             push(stack, result);
         }
     }
@@ -256,15 +433,12 @@ int calculate_expression_in_direct_polish_notation(string expression) {
         else if (elem == '+' || elem == '-' || elem == '*' || elem == '/') {
             int right = pop(stack);
             int left = pop(stack);
-            int result = for_calc(stack, elem, left, right);
+            int result = for_calc(elem, left, right);
             push(stack, result);
         }
     }
     return pop(stack);
 }
-void calculate_expression_in_simple_expression(string expression) {
-}
-void idz() {}
 int main() {
     setlocale(0, "");
     int identificator, i,res;
@@ -301,21 +475,57 @@ int main() {
             }
             break;
         case 3:
-            checking_simple_expression(expression);
-            checking_reverse_polish_notation(expression);
-            checking_direct_polish_notation(expression);
+            cout << "Выберите действие:" << endl;
+            cout << "1. Проверить простое выражение." << endl;
+            cout << "2. Проверить обратную польскую нотацию." << endl;
+            cout << "3. Проверить прямую польскую нотацию." << endl;
+            cin >> i;
+            switch (i) {
+            case 1:
+                cout << "Ваше простое выражение: ";
+                if (checking_simple_expression(expression)) {
+                    cout << "Correct" << endl;
+                }
+                else {
+                    cout << "Incorrect" << endl;
+                }
+                break;
+            case 2:
+                cout << "Ваше выражение в обратной польской нотации: ";
+                if (checking_reverse_polish_notation(reverse_pol_not)) {
+                    cout << "Correct" << endl;
+                }
+                else {
+                    cout << "Incorrect" << endl;
+                }
+                break;
+            case 3:
+                cout << "Ваше выражение в прямой польской нотации: ";
+                if (checking_direct_polish_notation(direct_pol_not)) {
+                    cout << "Correct" << endl;
+                }
+                else {
+                    cout << "Incorrect" << endl;
+                }
+                break;
+            }
             break;
         case 4:
-            cout << "Полученная  обратная польская нотация:  " << reverse_pol_not << endl;
-            res = calculate_expression_in_reverse_polish_notation(reverse_pol_not);
-            cout << res << endl;
+            cout << "Простое выражение:  " << expression << endl;
+            res = calculate_simple_expression(expression);
+            cout <<"Его результат: "<< res << endl;
             break;
         case 5:
-            cout << "Полученная прямая польская нотация:  " << direct_pol_not << endl;
-            res = calculate_expression_in_direct_polish_notation(direct_pol_not);
-            cout << res << endl;
+            cout << "Обратная польская нотация:  " << reverse_pol_not << endl;
+            res = calculate_expression_in_reverse_polish_notation(reverse_pol_not);
+            cout << "Её результат: " << res << endl;
             break;
         case 6:
+            cout << "Полученная прямая польская нотация:  " << direct_pol_not << endl;
+            res = calculate_expression_in_direct_polish_notation(direct_pol_not);
+            cout << "Её результат: " << res << endl;
+            break;
+        case 7:
             cout << "Выход!" << endl;
             return 0;
 
